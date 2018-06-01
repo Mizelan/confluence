c91d25b8ce81d1123eab03181dadc871a68e477a
mizelan@home-pc <mizelan@gmail.com>
2018-06-01 10:59:13 +0900

    데이터 복원 스크립트 개선

--- scripts/restore.ps1
+++ scripts/restore.ps1
@@ -3,15 +3,24 @@
 
 param([string]$Date)
 
-$BackupPath = "Backups/$Date"
+$BackupPath = "Backups/$Date.zip"
 $WikiContainerId = (docker ps -aqf name=confluence)
 $DBContainerId = (docker ps -aqf name=postgres)
 
 if([string]::IsNullOrWhitespace($Date) -or
     !(Test-Path -Path $BackupPath )) {
-    Write-Output "invalid backup path : $BackupPath"
+    Write-Output "invalid backup file : $BackupPath"
     Return -1
 }
+
+# 도커 컴포즈 실행
+Write-Output 'docker-compose up.'
+docker-compose up -d
+Start-Sleep -Seconds 5
+
+# 전체 압축 해제
+New-Item -ItemType Directory -Force -Path ./temp | Out-Null
+Expand-Archive $BackupPath -DestinationPath ./temp
 
 # 정보 표시
 Write-Output BackupPath=$BackupPath
@@ -19,9 +28,17 @@ Write-Output DBContainerId=$DBContainerId
 Write-Output WikiContainerId=$WikiContainerId
 
 # DB 복원
-#Get-Content $BackupPath/db.sql | docker exec -i $DBContainerId psql -U postgres
+Get-Content ./temp/$Date/db.sql | docker exec -i $DBContainerId psql -U postgres
+
+# 도커 컴포즈 중지
+Write-Output 'docker-compose stop.'
+docker-compose stop
 
 # 컨플루언스 복원
-Copy-Item -Force -Path $BackupPath\confluence.tgz -Destination ./temp/confluence.tgz
-docker exec -t --privileged $WikiContainerId tar xzvf /tmp/backup/confluence.tgz -C /var/atlassian
-Remove-Item ./temp/confluence.tgz
+Remove-Item ./confluencedata -Recurse -Force
+Expand-Archive ./temp/$Date/confluence.zip -DestinationPath ./
+Remove-Item ./temp/ -Recurse -Force
+
+# 도커 컴포즈 실행
+Write-Output 'docker-compose up.'
+docker-compose up -d
